0fcdb5f0da527a5666da73fe21ac991c7aab0f2a
David Chui <david.gchui@gmail.com>
2021-04-01 08:53:58 -0600

    Add files via upload
    Understanding error checking and try catch method

---
+++ Bullet Proof/Bullet Proof/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Bullet_Proof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = CheckDouble();
+            int y = CheckInt();
+        }
+        static double CheckDouble()
+        {
+            double x = 0;
+            bool succeed = false;
+            while (succeed == false)
+            {
+                try
+                {
+                    Console.WriteLine("Enter a double:");
+                    x = double.Parse(Console.ReadLine());
+                    succeed = true;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return x;
+        }
+        static int CheckInt()
+        {
+            int x = 0;
+            bool succeed = false;
+            while (succeed == false)
+            {
+                try
+                {
+                    Console.WriteLine("Enter an integer:");
+                    x = int.Parse(Console.ReadLine());
+                    succeed = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return x;
+        }
+    }
+}
